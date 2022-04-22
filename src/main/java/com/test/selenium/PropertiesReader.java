60168d31138664260d171eb8dc4e5aa1d34e4357
NishantSingh07 <nis.kr.singh@gmail.com>
2022-04-22 11:52:20 +0530

    Updates

--- src/main/java/com/test/selenium/PropertiesReader.java
+++ src/main/java/com/test/selenium/PropertiesReader.java
@@ -2,22 +2,24 @@ package com.test.selenium;
 import java.io.FileReader;
 import java.util.Properties;
 
+import org.checkerframework.checker.units.qual.s;
 public class PropertiesReader {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		String path1 = System.getProperty("user.dir") + "\\src\\main\\java\\config.properties";
+        String path="/Users/nishant/Desktop/Java/proj/src/main/java/com/application/myapp/Config.properties";
+        try {
+            FileReader fs = new FileReader(path);
+            Properties p = new Properties();
+            p.load(fs);
+            System.out.println("App URL is " + "\t" +  p.getProperty("url"));
+            System.out.println("User Name is " + "\t" +  p.getProperty("uname"));
+            System.out.println("Password is  " + "\t" +  p.getProperty("password"));
 
-		try {
-			FileReader fs = new FileReader(path1);
-			Properties p = new Properties();
-			p.load(fs);
-			System.out.println("App URL is " + "\t" + p.getProperty("url"));
-			System.out.println("User Name is " + "\t" + p.getProperty("uname"));
-			System.out.println("Password is  " + "\t" + p.getProperty("password"));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            
+        }
 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-	}
+    }
 }
--- src/main/java/com/test/selenium/TestBase.java
+++ src/main/java/com/test/selenium/TestBase.java
@@ -1,101 +1,72 @@
 package com.test.selenium;
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.*;
 import org.testng.annotations.*;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.Test;
+import java.util.Iterator;
+import java.util.Set;
 
+public class TestBase  {
 
-public class TestBase {
+    WebDriver driver;
 
-	WebDriver driver;
-	static Logger logger = LogManager.getLogger(TestBase.class);
-	//PropertyConfigurator.configure("C:\\Users\\GUR37160\\.git\\com.test.selenium\\src\\main\\java\\log4j.properties");	
-	
-	
+    @BeforeSuite
+    public void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-	@BeforeSuite
-	public void setupClass() {
-		logger.info("Setting up Webdriver ....!");
-		
-		logger.warn("This is a warning msg");
-		WebDriverManager.chromedriver().setup();
-	}
+    @BeforeTest
+    public void setupTest() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().deleteAllCookies();
+      //  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
 
-	@BeforeTest
-	public void setupTest() {
-		// driver = new SafariDriver();
-		
-		logger.info("Setting up ChromeDriver ....!");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().deleteAllCookies();
-		}
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://tripadvisor.com");
+        String title = driver.getTitle();
+        System.out.println(title);
+        driver.findElement(By.xpath("//a[@class='fGwNR _G B- z _S c Wc ddFHE fRPQK w fNnhN brHeh']")).click();
+        Thread.sleep(3000);
+        System.out.println("Click Success...");
+        Set<String> handles = driver.getWindowHandles(); // get all window handles
+        Iterator<String> iterator = handles.iterator();
+        String subWindowHandler=null;
+        while (iterator.hasNext()){
+            subWindowHandler = iterator.next();
+        }
+        driver.switchTo().window(subWindowHandler);// switch to popup window
+        System.out.println(subWindowHandler);
+        Thread.sleep(1200);
 
-	@Test(priority = 1)
-	public void test() throws InterruptedException {
-		logger.info("Fetching Website ....!");
-		driver.get("https://tripadvisor.com");
-		logger.info("Getting Title ....!");
-		String title = driver.getTitle();
-		System.out.println(title);
-		logger.info("Locating WebElement ....!");
-		driver.findElement(By.xpath("//a[@class='fGwNR _G B- z _S c Wc ddFHE fRPQK w fNnhN brHeh']")).click();
-		Thread.sleep(3000);
-		System.out.println("Click Success...");
-		
-		WebElement frame = driver.findElement(By.xpath("//iframe[@class=\"eBsLQ _R B- o\"]"));
-		driver.switchTo().frame(frame);		
-		System.out.println(driver.getTitle());
-		
-		JavascriptExecutor jse = ((JavascriptExecutor)driver);
+        // Now you are in the popup window, perform necessary actions here
 
-		WebElement email = driver.findElement(By.xpath("//span[text()='Continue with email']"));
-	
-		jse.executeScript("arguments[0].click();", email);		
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//input[@id='regSignIn.email']")).sendKeys("nis.kr.singh@gmail.com");
-		driver.findElement(By.xpath("//input[@id='regSignIn.password']")).sendKeys("Passw0rd12");
-		Thread.sleep(1200);
-		driver.findElement(By.xpath("//button[@class='ui_button primary coreRegPrimaryButton  regSubmitBtnEvent']"))
-				.click();
+        driver.findElement(By.xpath("//span[text()='Continue with email']")).click();
+        driver.findElement(By.xpath("//input[@id='regSignIn.email']")).sendKeys("nis.kr.singh@gmail.com");
+        driver.findElement(By.xpath("//input[@id='regSignIn.password']")).sendKeys("Passw0rd12");
+        Thread.sleep(1200);
+        driver.findElement(By.xpath("//button[@class='ui_button primary coreRegPrimaryButton  regSubmitBtnEvent']")).click();
+        //driver.switchTo().window(parentWindowHandler);  // switch back to parent window
 
-		System.out.println(title);
-		System.out.println("======================");
-	
-	}
-	
-	
-	@Test(priority = 2)
-	public void test2() throws InterruptedException {
-		
+        System.out.println(title);
+        System.out.println("======================");
+      //  System.out.println(reader.getCellData("Sheet1","Name",2));
+    }
 
-		System.out.println("THIS IS TEST2");
-		System.out.println("======================");
-	
-	}
-	
-	@Test(priority = 3)
-	public void test3() throws InterruptedException {
-		
+    @AfterTest
+    public void teardown() {
 
-		System.out.println("THIS IS TEST3");
-		System.out.println("======================");
-	
-	}
+        driver.close();
+        driver.quit();
 
-
-	@AfterTest
-	public void teardown() {
-
-		driver.close();
-		driver.quit();
-
-	}
+    }
 }
+
